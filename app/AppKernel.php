8940e0298567ab8ec1a36c78ccc04180a9aa4db1
lfelipeqn <lfelipeqn@lfelipeqn-N53SM>
2018-05-04 18:56:33 -0500

    -Generacion de Graficos para Clientes con google graphs -Reporte Detallado para Clientes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new CMEN\GoogleChartsBundle\CMENGoogleChartsBundle(),
             new AppBundle\AppBundle(),
         ];
 
